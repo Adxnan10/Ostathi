d3eb49686663a0ce76d083f417819298bcb63b77
Adxnan10 <Adnan66b@gmail.com>
2023-03-24 23:48:50 +0300

    Finished Sessions cards components

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,27 +1,39 @@
-import React, { useEffect, useState } from 'react'
+
 import SessionCardP from '../components/session/SessionCardP'
+import SessionCardR from '../components/session/SessionCardR'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 export default function HomePage() {
-  const [userData, setUserData] = useState({});
-  useEffect(() => {
-    fetch("http://localhost:3001/home")
-      .then(res => res.json())
-      .then(data => setUserData(data));
-  }, []);
+  const dummySessions = [{
+    topic: "Math",
+    duration: "2 hours",
+    title: "Linear Algebra",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, voluptate!",
+    img: "/Model.jpeg",
+    userName: "Yzd",
+    userImg: "/logo.svg",
+    price: 20
+  },
+  {
+    topic: "Algorithms",
+    duration: "1 hour",
+    title: "A* explained",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, voluptate!",
+    img: "/logo.svg",
+    userName: "Adnan",
+    userImg: "/Model.jpeg",
+    price: 30
+  }
+  ];
   return (
     <div className="App">
-      {
-        typeof userData === 'undefined' ? (<p>Loading..</p>) :
-          (<h1>Hi {userData.username}</h1>)
-      }
       <Container>
         <Row>
-          <Col><SessionCardP title="Adnan" /></Col>
-          <Col><SessionCardP title="Yzd" /></Col>
-          <Col><SessionCardP title="Ahmad" /></Col>
+          <Col><SessionCardP session={dummySessions[0]} /></Col>
+          <Col><SessionCardP session={dummySessions[1]} /></Col>
+          <Col><SessionCardR session={dummySessions[1]} /></Col>
         </Row>
 
       </Container>
