f5b470d6d138020befcb00b50375b221df4e2fdd
Ahmad <ahmad.badghaish1@gmail.com>
2023-03-25 17:22:04 +0300

    Footer- first commin

--- client/components/common/Footer.jsx
+++ client/components/common/Footer.jsx
@@ -1,5 +1,16 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer id="footer-common">
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <img class="col-3" src="logo.svg" alt="Logo" />  
+          <img class="col-3" src="vr.svg" alt="Logo" />  
+          <p class="col-3" id="footer-text"> Your new way of learning. </p>
+        </div>
+        <div class="row justify-content-center">
+          <p class="col-1"> © 2023 Ostathi Inc. </p>
+        </div>
+      </div>
+    </footer>
   )
 }
