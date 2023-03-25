3886287fe8a4292f7c349cec51430d47babc589b
Mubarak <mubarak.mb14@gmail.com>
2023-03-25 17:23:10 +0300

    start

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,5 +1,21 @@
 export default function Login() {
     return (
-      <div>Login</div>
+      <div>
+
+        <div class="text-center" style={{ width: 100+'%', height: 'auto' }}>
+            <img class="img-fluid" src="/logo.svg" alt="Logo" />
+        </div>
+
+            <div class="text-center" style={{ width: 50+'%', height: 'auto', float: 'left'}}>
+                <img class="img-fluid" src="/logo.svg" alt="Logo" />
+            </div>
+            <div class="text-center" style={{ width: 50+'%', height: 'auto', float: 'left'}}>
+                <div>
+                    
+                </div>
+            </div>
+        
+
+      </div>
     )
   }
