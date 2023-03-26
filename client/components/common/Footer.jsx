8cd67a33dc16936c4c5cbc47f7d081329d260906
Ahmad <40579808+AHMAD-879@users.noreply.github.com>
2023-03-26 23:17:46 +0300

    Footer finished 1.1 (#5)
    By Ahmad

--- client/components/common/Footer.jsx
+++ client/components/common/Footer.jsx
@@ -1,5 +1,25 @@
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+      <footer id="footer-common" className="footer-bs mt-auto">
+        <div className="container-fluid overflow-hidden">
+  
+          <div className="d-flex justify-content-center mb-3 align-items-center">
+            <img className="my-auto" id="footer-img" src="logo.svg" alt="Logo" />
+            <p className="my-auto footer-left-border footer-padding" id="footer-text"> Your new way of learning. </p>
+          </div>
+        
+          <div className="d-flex justify-content-center mb-5" style={{width: '40%', alignItems: 'center', margin: '0 auto'}}>
+            <a href="#" className="footer-links"> <p className="my-auto footer-padding"> Careers </p> </a>
+            <a href="#" className="footer-links"> <p className="my-auto footer-padding footer-left-border"> Privacy Policy </p> </a>
+            <a href="#" className="footer-links"> <p className="my-auto footer-padding footer-left-border"> Terms & Conditions </p> </a>
+          </div>
+  
+          <div className="d-flex justify-content-center mb-3">
+            <p className="my-auto"> © 2023 Ostathi Inc. </p>
+          </div>
+  
+        </div>
+      </footer>
+    )
+  }
+   
