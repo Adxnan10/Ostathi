560cdea4aae295821510f31076abb83947cf65fd
YazeedAlsaeed <alsaeedyazid@gmail.com>
2023-03-26 08:06:28 +0300

    finished session page

--- client/pages/sessionDetails.jsx
+++ client/pages/sessionDetails.jsx
@@ -1,14 +1,35 @@
 import Container from 'react-bootstrap/Container';
+import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { BsClockHistory, BsGrid } from 'react-icons/bs'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import ReactDOM from 'react-dom/client';
 
+//Provide Rating, Pics, Session Details, Dates, Names, etc.
 
 export default function SessionDetails(){
+    var rate = "none";
+    var overview = "block";
+    const rating = [1500,1000,2555,2300,2500]
+    var total = 0;
+    rating.forEach(element => {
+        total =+ element;
+    });
+    const comments = [{
+        name:"Yazeed",
+        comment:"Nice tutor",
+        date: "May 24, 2022"
+    } , {
+        name:"Yazeed",
+        comment:"Nice tutor",
+        date: "May 24, 2022"
+    }]
+
     return(<>
     <div className="sessionBackGrnd"/>
-
     <Container>
        <Row>
         <Col lg="8">
@@ -17,22 +38,23 @@ export default function SessionDetails(){
                <Button className="btn btn-primary optionSessionPage" id='overviewSession'>
                         Overview
                     </Button>
-                    <Button className="btn btn-primary optionSessionPage" id='ratingSession'>
-                        Rating
+                    <Button className="btn btn-primary optionSessionPage" id='ratingSession' onClick={(overview) => overview = "none"}>
+                        {overview}
                     </Button>
                </Col>
                    
             </Row>
             <Row>
                 <div id='sessionDescription'>
-                    <Row>
+                    <div className='sessionOverview' style={{display: overview}}>
+                            <Row>
                         <div className='datailsSession'>
                                 <div className='datailsSession'>
                                     <img src="/Model.jpeg" alt="Model" id='sessionPics'/>
                                     <p id='nameSessionDet'>Yazeed</p >
                                 </div>
                                 <div className='datailsSession'>
-                                    <p>ICON HERE</p>
+                                    <BsGrid className='BsGrid'/>
                                     <p>&ensp;4th of May 2022</p>
                                 </div>
                         </div>
@@ -46,6 +68,55 @@ export default function SessionDetails(){
                         Software Engineering SE2014 guidelines and meets ABET’s Engineering Accreditation Criteria (EAC). 
                         </p>
                     </Row>
+                    </div>
+                    <div className='sessionRating' style={{display: rate}}>
+                    <Row>
+                        <Col sm="12" md="6" lg="4" xlg="4">
+                            <div className='ratingBox'>
+                            <h4>4 out of 5</h4>
+                            <p>Top rating</p>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className='ratingBars'>
+                            {rating.map((value,index) => {
+                                    return(
+                                        <Row>
+                                    <Col md="6" lg="2">
+                                        <p>{index+1} stars</p>
+                                        </Col> 
+                                        <Col>
+                                        <div className='ratingBar'><ProgressBar variant='warning' className='ratingBar' now={(value/total) * 100}/></div>
+                                        </Col>
+                                        </Row>
+                                    );
+                            })
+                            } 
+                            </div>
+                        </Col>
+                    </Row>
+                        {comments.map((value,index) => {
+                            return(
+                            <><Row>
+                            <div className='datailsSession'>
+                                    <div className='datailsSession'>
+                                        <img src="/Model.jpeg" alt="Model" id='sessionPics'/>
+                                        <p id='nameSessionDet'>{value.name}</p >
+                                    </div>
+                                    <div className='datailsSession'>
+                                        <BsGrid className='BsGrid'/>
+                                        <p>&ensp;{value.date}</p>
+                                    </div>
+                            </div>
+                        </Row>
+                        <Row className='datailsSession'>
+                            <p>{value.comment}
+                            </p>
+                        </Row>
+                        {comments.length - index - 1 == 0? <></>: <hr/>} 
+                        </>
+                        );})} 
+                    </div>
                 </div>
             </Row>
         </Col>
@@ -64,13 +135,13 @@ export default function SessionDetails(){
                     <h4 id='titleSesDet'><b>This Course Includes</b></h4>
                     <div id='greyGuarnt'>
                     <div>
-                        <p>Money Back Guarantee</p>
+                        <p><BsGrid className='BsGrid'/>Money Back Guarantee</p>
                     </div>
                     <div>
-                    <p>Access on all devices</p>
+                    <p><BsGrid className='BsGrid'/>Access on all devices</p>
                     </div>
                     <div>
-                        <p>Certification of completion</p>
+                        <p><BsGrid className='BsGrid'/>Certification of completion</p>
                     </div>
                     </div>
                     
