526260704bae82fc90152b513e434b2be28d1895
Adxnan10 <Adnan66b@gmail.com>
2023-03-27 22:37:22 +0300

    customize 404 page

---
+++ client/pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="404-block d-flex justify-content-center align-items-center" style={{ height: '60vh' }}>
+      <h1 style={{ color: "#023047" }}>404 - please Ostathi, make sure of the url!</h1>
+    </div>
+  )
+}
