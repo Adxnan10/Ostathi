ddb5935f16a44eb29bd2cc25a29f83b12d8ea057
Adnan <adnan66b@gmail.com>
2023-03-28 20:26:30 +0300

    Edit session card styles

--- client/components/session/sessionCardR.jsx
+++ client/components/session/sessionCardR.jsx
@@ -16,7 +16,8 @@ export default function SessionCardR({ ...props }) {
     price: 20
    */
   return (
-    <Card className="session-card session-card-posted" style={{ width: '20rem', borderRadius: '1rem' }}>
+    <Card className="session-card session-card-requested" style={{ width: '20rem', borderRadius: '1rem' }}>
+      <div className="indicator">requested</div>
       <Card.Img className="session-card-img" variant="top" src={props.session.img} />
       <Card.Body>
         <div className="session-info d-flex justify-content-around" style={{ color: "#666976" }}>
@@ -27,13 +28,14 @@ export default function SessionCardR({ ...props }) {
             <BsClockHistory /><span>{` ${props.session.duration}`}</span>
           </div>
         </div>
+
         <Card.Title>{props.session.title}</Card.Title>
         <Card.Text style={{ color: "#666976" }}>
-          {props.session.text}
+          {props.session.text.slice(0, 100)} <span style={{ color: '#6E7BAF' }}>...Read more</span>
         </Card.Text>
         <div className="d-flex justify-content-between align-items-center">
           <div className='user-info'>
-            <img src={props.session.userImg} alt="student pic" className='user-pic rounded-pic' style={{ maxHeight: '60px' }} />
+            <img src={props.session.userImg} alt="student pic" className='user-pic rounded-pic' />
             <span>{props.session.userName}</span>
           </div>
           <div className='session-bid' style={{ color: "#F5931C" }} >
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -6,36 +6,11 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 export default function HomePage() {
-  const dummySessions = [{
-    topic: "Math",
-    duration: "2 hours",
-    title: "Linear Algebra",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, voluptate!",
-    img: "/Model.jpeg",
-    userName: "Yzd",
-    userImg: "/logo.svg",
-    price: 20
-  },
-  {
-    topic: "Algorithms",
-    duration: "1 hour",
-    title: "A* explained",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, voluptate!",
-    img: "/logo.svg",
-    userName: "Adnan",
-    userImg: "/Model.jpeg",
-    price: 30
-  }
-  ];
   return (
     <div className="App">
       <Container>
         <Row>
-          <Col><SessionCardP session={dummySessions[0]} /></Col>
-          <Col><SessionCardP session={dummySessions[1]} /></Col>
-          <Col><SessionCardR session={dummySessions[1]} /></Col>
         </Row>
-
       </Container>
     </div>
   );
