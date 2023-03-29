aacf34043b8e00572817572d6139d65967d3940d
Ahmad <ahmad.badghaish1@gmail.com>
2023-03-30 01:21:35 +0300

    Post Session Done

--- client/pages/postSession.jsx
+++ client/pages/postSession.jsx
@@ -13,14 +13,42 @@ export default function PostSession() {
         </div>
 
         <div className="container">
-            <form className="form-group">
+            <form className="form-post">
                 <label class="row" htmlFor="Subject"> Session Subject </label>
-                <select class="row" name="Subject" placeholder="Software Engineering">
+                <select class="row full-width" name="Subject" placeholder="Software Engineering">
                     <option value="Software Engineering" selected> Software Engineering </option>
                     <option value="Mathematics"> Mathematics </option>
                     <option value="Physics"> Physics </option>
                     <option value="Chemistry"> Chemistry </option>
                 </select>
+                <label class="row" htmlFor="Title"> Session Title </label>
+                <input class="row full-width" type="text" name="Title" placeholder="Introduction to Web Development and Engineering" />
+                <section class="row d-flex justify-content-between">
+                    <div className="col">
+                        <label class="row" htmlFor="StartDate"> Start Date / Time </label>
+                        <input class="row half-width" type="datetime-local" name="StartDate"/>
+                    </div>
+                    <div className="col">
+                        <label class="row" htmlFor="EndDate"> End Date / Time </label>
+                        <input class="row half-width" type="datetime-local" name="EndDate"/>
+                    </div>
+                </section>
+                <section class="row d-flex justify-content-between">
+                    <div className="col">
+                        <label class="row" htmlFor="Type"> Type </label>
+                        <select class="row half-width" name="Type"  >
+                            <option value="One-to-one" selected> One to one meeting </option>
+                            <option value="Group"> Group meeting </option>
+                        </select>
+                    </div>
+                    <div className="col">
+                        <label class="row" htmlFor="Price"> Price </label>
+                        <input class="row half-width" type="number" name="Price" placeholder="SAR" min={0} step={0.01} />
+                    </div>
+                </section>
+                <label class="row" htmlFor="Description"> Session Description </label>
+                <textarea class="row full-width" name="Description" rows={7} placeholder="Enter a description for your session" />
+                <button class="activeButtonDS btn btn-danger" type="submit" id="postButton"> Post Session </button>
             </form>
         </div>
     </div>
