9ed1e3c2ad35d41baf59e8eede78b551af23af89
Adxnan10 <Adnan66b@gmail.com>
2023-04-29 13:49:03 +0300

    prepare db

---
+++ client/pages/apiTester.jsx
@@ -0,0 +1,17 @@
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+export default function Tester() {
+  const { data, error, isLoading } = useSWR('/api/testapi/mockapi', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div>
+      <h1>{data.Title}</h1>
+      <p>this title: {data.Title} is for session # {data.id}</p>
+    </div>
+  )
+}
